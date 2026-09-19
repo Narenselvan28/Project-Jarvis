df96c70ec2481d316b2b59452ab6d1a7a719a15b
Naren Selvan <narenselvan77@gmail.com>
2026-09-19 06:24:41 +0530

    fix(wsgi): allow unsafe werkzeug for local run

--- wsgi.py
+++ wsgi.py
@@ -16,4 +16,4 @@ app = create_app()
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=False)
+    socketio.run(app, host="0.0.0.0", port=port, debug=False, allow_unsafe_werkzeug=True)
